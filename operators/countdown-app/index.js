c94570d895c23cc3041983cb5d6dd404f5f86528
vovapankiw <vovapankiw@gmail.com>
2021-04-29 10:36:46 +0300

    added filtering operators

--- operators/countdown-app/index.js
+++ operators/countdown-app/index.js
@@ -2,11 +2,16 @@
 const counter$ = rxjs.interval(1000);
 const countDown = document.getElementById('countdown');
 const message = document.getElementById('message');
+const abortBtn = document.getElementById('abort');
+
+const click$ = rxjs.fromEvent(abortBtn, 'click');
 
 counter$.pipe(
   rxjs.operators.mapTo(-1),
   rxjs.operators.scan((acc, curr) => acc + curr, 10),
-  rxjs.operators.filter(val => val >= 0)
+  rxjs.operators.filter(val => val >= 0),
+  rxjs.operators.takeWhile(val => val >= 0),
+  rxjs.operators.takeUntil(click$)
 ).subscribe(val => {
   countDown.innerText = val;
 
